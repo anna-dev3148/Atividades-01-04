64ff3ff2d8487f96f56ed1c0930efb19c21439ec
Anna Letícia Silva <annaleticiabarbosadasilva@gmail.com>
2025-04-02 16:53:23 -0300

    Create ex5.c

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    for(int a = 0; a < 2; a++) {
+        for(int b = 0; b < 2; b++) {
+            printf("\nA: %d ", a);
+            printf("\nB: %d\n ", b);
+            printf("\nA e B: %d || %d = %d", a, b,a || b);
+        }
+    }
+  return 0;
+}
